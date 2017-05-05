0eb56cd6a14cf4352cb60070670516912dc307ef
Miro Nieminen <miro.nieminen@futurice.com>
2017-05-05 22:22:54 +0300

    Simplify main view a little bit

--- src/elm/App/View.elm
+++ src/elm/App/View.elm
@@ -16,27 +16,20 @@ import Components.Modal
 
 view : Model -> Html Msg
 view model =
-    let
-        codeString =
-            getCodeString Code.code model.codePosition
-
-        newCoder =
-            { ebitRate = 2, cost = 100 }
-    in
-        div []
-            [ div [ class "container-fluid background" ]
-                [ div [ class "row" ]
-                    [ div [ class "col-md-4 container" ]
-                        [ Components.Stats.view model
-                        , Components.HireButton.view
-                        , button [ class "btn btn-primary", onClick <| UpgradeOffice 2 400 ] [ text "Upgrade Office" ]
-                        ]
-                    , div [ class "col-md-6" ]
-                        [ Components.CodeBlock.view codeString ]
-                    ]
-                ]
-            , Components.Modal.view model.applicant
-            ]
+      div []
+          [ div [ class "container-fluid background" ]
+              [ div [ class "row" ]
+                  [ div [ class "col-md-4 container" ]
+                      [ Components.Stats.view model
+                      , Components.HireButton.view
+                      , button [ class "btn btn-primary", onClick <| UpgradeOffice 2 400 ] [ text "Upgrade Office" ]
+                      ]
+                  , div [ class "col-md-6" ]
+                      [ Components.CodeBlock.view ( getCodeString Code.code model.codePosition) ]
+                  ]
+              ]
+          , Components.Modal.view model.applicant
+          ]
 
 
 getCodeString : String -> Int -> String
