776a29ab17f73fef205fec1a0a96464bd0ec54d8
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2017-05-05 11:40:08 +0300

    add ebit code field

--- src/elm/App/View.elm
+++ src/elm/App/View.elm
@@ -1,9 +1,11 @@
 module App.View exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import App.Types exposing (Msg(..), Model)
 import Html.Events exposing (onClick)
 import Components.EbitButton as EbitButton
+import Code
 
 
 view : Model -> Html Msg
@@ -14,4 +16,12 @@ view model =
             ]
         , button [ onClick <| AddCoder { ebitRate = 2, cost = 1 } ] [ text "foo" ]
         , EbitButton.view model
+        , div []
+            [ pre
+                []
+                [ Html.code
+                    [ class "java" ]
+                    [ text <| String.slice 0 (model.ebit * 10) Code.code ]
+                ]
+            ]
         ]
